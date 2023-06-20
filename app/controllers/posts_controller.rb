5d566df32f1cccc9d74c41e44166a2acaf1f9712
Yehwan Baek <baekokok98@gmail.com>
2023-06-20 18:34:11 +1000

    added code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,14 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_record_response
 
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
   end
 
@@ -20,4 +18,7 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
+  def render_invalid_record_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
 end
